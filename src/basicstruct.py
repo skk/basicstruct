17e8d12facb8547d2b21d7492a6e8b22dce124d6
Steven Knight <steven.knight@cashstar.com>
2015-10-22 16:17:42 -0400

    Main changes:
    * Added JSON encoding/decoding.
    * Added to_dict, to_ordered_dict
    * Renamed _to_tuple to to_tuple

--- src/basicstruct.py
+++ src/basicstruct.py
@@ -2,9 +2,11 @@
 # This program is distributed under the MIT license.
 
 """This is a placeholder."""
+import json
+from json.decoder import WHITESPACE
+import collections
 
 import six
-from six.moves import zip
 from itertools import chain
 
 __version__ = '1.0.3'
@@ -16,6 +18,8 @@ class BasicStruct(object):
     __slots__ = ()  # should be extended by deriving classes
 
     def __init__(self, *args, **kwargs):
+        super(BasicStruct, self).__init__()
+
         arg_pairs = zip(self.__slots__, args)
         for key, value in chain(arg_pairs, six.iteritems(kwargs)):
             setattr(self, key, value)
@@ -32,43 +36,113 @@ class BasicStruct(object):
     def __lt__(self, other):
         if not isinstance(other, type(self)):
             return NotImplemented
-        return self._to_tuple() < other._to_tuple()
+        return self.to_tuple() < other.to_tuple()
 
     def __le__(self, other):
         if not isinstance(other, type(self)):
             return NotImplemented
-        return self._to_tuple() <= other._to_tuple()
+        return self.to_tuple() <= other.to_tuple()
 
     def __gt__(self, other):
         if not isinstance(other, type(self)):
             return NotImplemented
-        return self._to_tuple() > other._to_tuple()
+        return self.to_tuple() > other.to_tuple()
 
     def __ge__(self, other):
         if not isinstance(other, type(self)):
             return NotImplemented
-        return self._to_tuple() >= other._to_tuple()
+        return self.to_tuple() >= other.to_tuple()
 
     def __eq__(self, other):
         if not isinstance(other, type(self)):
             return NotImplemented
-        return self._to_tuple() == other._to_tuple()
+        return self.to_tuple() == other.to_tuple()
 
     def __ne__(self, other):
         if not isinstance(other, type(self)):
             return NotImplemented
-        return self._to_tuple() != other._to_tuple()
+        return self.to_tuple() != other.to_tuple()
 
-    def __hash__(self):
-        return hash(self._to_tuple())
+    def __len__(self):
+        return len(self.__slots__)
 
-    def _to_tuple(self):
-        return tuple(getattr(self, key) for key in self.__slots__)
+    def __iter__(self):
+        for key in self.__slots__:
+            yield key
+
+    def __getitem__(self, key):
+        if hasattr(self, key):
+            val = getattr(self, key)
+            return val
+        else:
+            raise KeyError("key {} not found".format(key))
 
     def __getstate__(self):
-        return self._to_tuple()
+        d = {}
+        for attr in self.__slots__:
+            val = getattr(self, attr)
+            d[attr] = val
+
+        return d
 
     def __setstate__(self, state):
-        for key, value in zip(self.__slots__, state):
-            setattr(self, key, value)
+        for attr in self.__slots__:
+            if attr in state:
+                val = state[attr]
+                setattr(self, attr, val)
+
+    def __hash__(self):
+        return hash(self.to_tuple())
+
+    def to_tuple(self):
+        return tuple(getattr(self, key) for key in self.__slots__)
+
+    def to_dict(self):
+        return {
+            key: getattr(self, key)
+            for key in self.__slots__
+        }
+
+    def to_ordered_dict(self):
+        d = collections.OrderedDict()
+        for key in sorted(self.__slots__):
+            d[key] = getattr(self, key)
+
+        return d
+
+
+class BasicStructEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, BasicStruct):
+
+            data = {}
+            for key in obj.__slots__:
+                val = getattr(obj, key)
+
+                if isinstance(val, BasicStruct):
+                    val = BasicStructEncoder.default(self, val)
+
+                data[key] = val
+
+            return data
+
+        # Let the base class default method raise the TypeError
+        try:
+            return json.JSONEncoder.default(self, obj)
+        except TypeError, e:
+            raise e
+
+class BasicStructDecoder(json.JSONDecoder):
+    def decode(self, obj, _w=WHITESPACE.match):
+        json_obj = json.loads(obj)
+
+        class JSONDecoerTest(BasicStruct):
+            __slots__ = json_obj.keys()
+
+        basic_struct_obj = JSONDecoerTest()
+
+        for k, v in json_obj.iteritems():
+            setattr(basic_struct_obj, k, v)
+
+        return basic_struct_obj
 
